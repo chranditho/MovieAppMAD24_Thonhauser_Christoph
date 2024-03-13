a238c5aa4a3f14a615af9eaa3cfe1182af492e7b
christophthonhauser <c.thonhauser97@gmail.com>
2024-03-13 19:31:31 +0100

    move scaffolding to main activity

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -3,27 +3,92 @@ package com.example.movieappmad24
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.example.movieappmad24.models.getMovies
-import com.example.movieappmad24.ui.screens.DetailScreen
+import androidx.compose.ui.text.style.TextAlign
+import com.example.movieappmad24.ui.screens.HomeScreen
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MovieAppMAD24Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    val movie = getMovies().first() // Replace with actual movie object
-                    val navController = rememberNavController()
-                    DetailScreen(movie, navController)
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                titleContentColor = MaterialTheme.colorScheme.primary,
+                            ), title = {
+                                Text(
+                                    "Movie App",
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            })
+                        },
+                        bottomBar = {
+                            BottomAppBar(
+                                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                contentColor = MaterialTheme.colorScheme.primary,
+                            ) {
+                                NavigationBar {
+                                    NavigationBarItem(selected = true,
+                                        onClick = { /*TODO*/ },
+                                        icon = {
+                                            Icon(
+                                                imageVector = Icons.Default.Home,
+                                                contentDescription = "Home"
+                                            )
+                                        },
+                                        label = {
+                                            Text("Home")
+                                        })
+                                    NavigationBarItem(selected = false,
+                                        onClick = { /*TODO*/ },
+                                        icon = {
+                                            Icon(
+                                                imageVector = Icons.Default.Star,
+                                                contentDescription = "Watchlist"
+                                            )
+                                        },
+                                        label = {
+                                            Text("Watchlist")
+                                        })
+                                }
+                            }
+                        },
+                    ) { innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        ) {
+                            HomeScreen()
+                        }
+                    }
+
                 }
             }
         }
--- app/src/main/java/com/example/movieappmad24/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/ui/screens/DetailScreen.kt
@@ -1,25 +1,6 @@
 package com.example.movieappmad24.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -28,50 +9,9 @@ import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.components.MovieRow
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(movie: Movie, navController: NavController) {
-    Scaffold(
-        topBar = {
-            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.primary,
-            ), title = {
-                Text(
-                    "Movie App",
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth()
-                )
-            })
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary,
-            ) {
-                NavigationBar {
-                    NavigationBarItem(selected = true, onClick = { /*TODO*/ }, icon = {
-                        Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
-                    }, label = {
-                        Text("Home")
-                    })
-                    NavigationBarItem(selected = false, onClick = { /*TODO*/ }, icon = {
-                        Icon(imageVector = Icons.Default.Star, contentDescription = "Watchlist")
-                    }, label = {
-                        Text("Watchlist")
-                    })
-                }
-            }
-        },
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-        ) {
-            MovieRow(movie)
-        }
-    }
+    MovieRow(movie)
 }
 
 @Preview
