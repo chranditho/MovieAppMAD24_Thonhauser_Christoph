18ece4df465dad8831eb8785cf736cbd74459817
christophthonhauser <c.thonhauser97@gmail.com>
2024-04-07 19:37:51 +0200

    make watchlist screen scrollable

--- app/src/main/java/com/example/movieappmad24/ui/screens/WatchlistScreen.kt
+++ app/src/main/java/com/example/movieappmad24/ui/screens/WatchlistScreen.kt
@@ -1,6 +1,7 @@
 package com.example.movieappmad24.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import com.example.movieappmad24.ui.components.MovieRow
 import com.example.movieappmad24.viewmodels.MovieViewModel
@@ -9,8 +10,8 @@ import com.example.movieappmad24.viewmodels.MovieViewModel
 fun WatchlistScreen(viewModel: MovieViewModel) {
     val movies = viewModel.getFavoriteMovies()
 
-    Column {
-        movies.forEach { movie ->
+    LazyColumn {
+        items(movies) { movie ->
             MovieRow(movie, viewModel::isFavorite, viewModel::toggleFavorite)
         }
     }
