90ddc220115f903a03be79dd24b1012c32d2325a
christophthonhauser <c.thonhauser97@gmail.com>
2024-04-07 19:17:49 +0200

    add trailer

--- app/src/main/java/com/example/movieappmad24/viewmodels/MovieViewModel.kt
+++ app/src/main/java/com/example/movieappmad24/viewmodels/MovieViewModel.kt
@@ -1,14 +1,15 @@
 package com.example.movieappmad24.viewmodels
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
 
 class MovieViewModel : ViewModel() {
-    val movies = getMovies().map { movie ->
-        movie to mutableStateOf(movie.isFavorite)
-    }.toMap().toMutableMap()
+    private val movies = getMovies().associateWith { movie ->
+        mutableStateOf(movie.isFavorite)
+    }.toMutableMap()
 
     fun toggleFavorite(movie: Movie) {
         movies[movie]?.value = !(movies[movie]?.value ?: false)
